4dc70f99c92538f3c2cba196f21cb3aed452099c
vc-dhavaljoshi <dhaval.joshi@viitor.cloud>
2019-06-16 03:18:21 +0530

    Message destroy page changes

--- resources/views/message/message-view.blade.php
+++ resources/views/message/message-view.blade.php
@@ -3,15 +3,23 @@
     <section class="page-section home-page">
         <div class="container-fluid ">
             <div class="row justify-content-center">
-                <div class="col-md-4 noted-card">
-                    <div class="card">
-                        <div class="card-header">Dashboard</div>
+                @if($message != '')
+                    <div class="col-md-4 noted-card">
+                        <div class="card">
+                            <div class="card-header">Message</div>
+                            <div class="card-body">
+                                {{ $message }}
+                            </div>
 
-                        <div class="card-body">
-                            {{ $message ?? 'This message not longer available' }}
                         </div>
                     </div>
-                </div>
+                @else
+                    <div class="no-data">
+                        <div class="no-data-box">
+                            This message not longer available
+                        </div>
+                    </div>
+                @endif
             </div>
         </div>
     </section>
